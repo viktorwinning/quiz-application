efdd2e5525512e07035bbfc7daf421fef8f47a34
viktorwinning <vityakaluzhnij@gmail.com>
2025-12-08 20:20:13 +0100

    highscores

--- scripts/end.js
+++ scripts/end.js
@@ -1,6 +1,8 @@
 const mostRecentScore=localStorage.getItem('mostRecentScore');
+const highScores=JSON.parse(localStorage.getItem('highScores')) || [];
 const username=$('#username');
 const saveScoreBtn=$('#saveScoreBtn');
+const maxHighScores=5;
 
 $('#finalScore').text(mostRecentScore);
 
@@ -9,5 +11,31 @@ username.keyup(function(){
 })
 
 saveScoreBtn.click(function(){
-    console.log("Save score!")
+    const score={
+        score:mostRecentScore,
+        name:username.val(),
+
+    };
+    if (highScores.length<maxHighScores || mostRecentScore>highScores[maxHighScores-1].score){
+        highScores.push(score);
+        highScores.sort(function(a,b){
+            return b.score-a.score;
+        });
+        highScores.splice(maxHighScores);
+
+    }
+    else if(mostRecentScore<highScores[maxHighScores-1].score){
+        alert('Last score wasnt saved as its less than your top 5')
+    }
+    else{
+        for(let i=0;i<highScores.length-1;i++){
+            if(highScores[i].score===mostRecentScore){
+                highScores[i]=score;
+            break;
+        }   
+    }
+    }
+    console.log(highScores);
+    localStorage.setItem('highScores',JSON.stringify(highScores));
+    window.location.assign('index.html');
 })
