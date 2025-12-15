64e55dd7a69f519fdd12204e090df93a8463a98d
viktorwinning <vityakaluzhnij@gmail.com>
2025-12-15 19:56:29 +0100

    highscores_list

---
+++ scripts/highscores.js
@@ -0,0 +1,9 @@
+const highScores=JSON.parse(localStorage.getItem("highScores")) || [];
+$('#highScoresList').html(formHighScoresList(highScores));
+function formHighScoresList(highScores){
+    let scoreList="";
+    for(let highScore of highScores){
+        scoreList+=`<li class="high-score">${highScore.name}-${highScore.score}</li>`;
+    }
+    return scoreList;
+}
